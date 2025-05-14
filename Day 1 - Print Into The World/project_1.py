06cf7c765e2bd535dcf6e891135b82db1a046046
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-05-14 13:38:28 +0200

    Create project_1.py

---
+++ Day 1 - Print Into The World/project_1.py
@@ -0,0 +1,6 @@
+print("Marta")
+print("14.05.2025")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, 
+for a minimum of 30 minutes a day.""")
+print("I am feeling 🥳")
