757409e7d6d48bb5abff12701d5e189d1602ebf3
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-07-14 18:50:04 +0200

    Create project_15.py

---
+++ Day 15 - Animal Sound/project_15.py
@@ -0,0 +1,17 @@
+exit = ""
+while exit != "yes":
+  animal = input("What animal sound do you want to hear? ").lower()
+  if animal == "cow":
+    print("A cow goes moo.")
+  elif animal == "dog":
+    print("A dog goes woof.")
+  elif animal == "cat":
+    print("A cat goes meow.")
+  elif animal == "sheep":
+    print("A sheep goes baa.")
+  elif animal == "pig":
+    print("A pig goes oink.")
+  else: 
+    print("I don't know that animal sound. Try again.")
+  
+  exit = input("Do you want to exit? ").lower()
