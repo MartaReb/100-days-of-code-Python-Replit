895910fd0e4c777a0b6047f83bcfe945be97499b
MartaReb <49365894+MartaReb@users.noreply.github.com>
2025-08-24 10:44:55 +0200

    Create project_57.py

---
+++ Day 57 - Factorial Finder/project_57.py
@@ -0,0 +1,9 @@
+def factorial(number):
+  if number == 1:
+    return 1
+  else:
+    return number * factorial(number - 1)
+
+print("Factorial Finder")
+number = int(input("Input a number > "))
+print(factorial(number))
